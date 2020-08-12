c47ec7263ede235e5555988403439d1902648090
guisalmeida <guilherme.dasilva@linx3.com>
2020-08-11 21:49:27 -0300

    chore: first commit

---
+++ src/Spotify.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'YOUR_TOKEN_HERE',
+});
+
+export default spotify;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './main.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './example'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './example'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+        }],
+      },
+    ],
+  },
+};
